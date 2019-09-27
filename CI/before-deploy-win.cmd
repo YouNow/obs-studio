9753adbef1835e2e36b7518ceb44a6d92ab0c582
Roman Sivriver <roman@rsiv.net>
2019-09-27 10:50:48 -0400

    CI: fix package publishing

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -2,4 +2,4 @@ REM robocopy .\build32\rundir\RelWithDebInfo .\build\ /E /XF .gitignore
 REM robocopy .\build64\rundir\RelWithDebInfo .\build\ /E /XC /XN /XO /XF .gitignore
 REM 7z a build.zip .\build\*
 
-robocopy .\build64\rundir\RelWithDebInfo .\build\ *.msi *.zip
+robocopy .\build64 .\build\ *.msi *.zip
