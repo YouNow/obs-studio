51aa98be48481b42b9c60ec5656e81c92c877864
Roman Sivriver <roman@rsiv.net>
2019-09-27 10:15:57 -0400

    CI: publish only package artifacts in windows builds

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,3 +1,5 @@
-robocopy .\build32\rundir\RelWithDebInfo .\build\ /E /XF .gitignore
-robocopy .\build64\rundir\RelWithDebInfo .\build\ /E /XC /XN /XO /XF .gitignore
-7z a build.zip .\build\*
+REM robocopy .\build32\rundir\RelWithDebInfo .\build\ /E /XF .gitignore
+REM robocopy .\build64\rundir\RelWithDebInfo .\build\ /E /XC /XN /XO /XF .gitignore
+REM 7z a build.zip .\build\*
+
+robocopy .\build64\rundir\RelWithDebInfo .\build\ *.msi *.zip
